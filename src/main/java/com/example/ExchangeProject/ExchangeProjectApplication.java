5860b0adfeb0be5d2a5324961576abc84e2c14f2
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2024-07-17 15:56:58 +0900

    feat: ExchangeProjectApplication 수정 - 애플리케이션 실행 시 DB 데이터 유무 검증 - 병렬 처리 시간 측정 로직 추가

--- src/main/java/com/example/ExchangeProject/ExchangeProjectApplication.java
+++ src/main/java/com/example/ExchangeProject/ExchangeProjectApplication.java
@@ -1,13 +1,30 @@
 package com.example.ExchangeProject;
 
+import com.example.ExchangeProject.service.DataLoaderService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExchangeProjectApplication {
+public class ExchangeProjectApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+	private final DataLoaderService dataLoaderService;
+
+    public ExchangeProjectApplication(DataLoaderService dataLoaderService) {
+        this.dataLoaderService = dataLoaderService;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(ExchangeProjectApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		long startTime = System.currentTimeMillis();
+
+		dataLoaderService.loadDataIfNecessary();
+
+		long endTime = System.currentTimeMillis();
+		System.out.println("Parallel processing time: " + (endTime - startTime) + "ms");
+	}
 }
