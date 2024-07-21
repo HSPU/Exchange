32a2a3b0765b838a2b1f8dad8e0dc7b7c4dfd8a9
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2024-07-21 15:19:52 +0900

    feat: DataLoaderService 수정 - Mob 데이터 로드 추가

--- src/main/java/com/example/ExchangeProject/service/DataLoaderService.java
+++ src/main/java/com/example/ExchangeProject/service/DataLoaderService.java
@@ -1,24 +1,28 @@
 package com.example.ExchangeProject.service;
 
 import com.example.ExchangeProject.repository.ItemRepository;
+import com.example.ExchangeProject.repository.MobRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DataLoaderService {
 
     private final ItemRepository itemRepository;
     private final ItemService itemService;
-
-    public DataLoaderService(ItemRepository itemRepository, ItemService itemService) {
-        this.itemRepository = itemRepository;
-        this.itemService = itemService;
-    }
+    private final MobRepository mobRepository;
+    private final MobService mobService;
 
     public void loadDataIfNecessary() {
         // DB에 데이터가 있는지 검사
         if (itemRepository.count() == 0) {
             // 데이터가 없다면 api 호출 및 데이터 저장
             itemService.loadDataFromApi();
+        }
+
+        if (mobRepository.count() == 0) {
+            mobService.loadDataFromApi();
         }
     }
 }
