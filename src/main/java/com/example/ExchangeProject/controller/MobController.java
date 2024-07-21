2dc014753b5bbc06d22216fcfe22219565d64d39
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2024-07-21 15:19:04 +0900

    feat: MobController 수정 - RestController 로 수정

--- src/main/java/com/example/ExchangeProject/controller/MobController.java
+++ src/main/java/com/example/ExchangeProject/controller/MobController.java
@@ -3,16 +3,12 @@ package com.example.ExchangeProject.controller;
 import com.example.ExchangeProject.entity.Mob;
 import com.example.ExchangeProject.service.MobService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/api/mobs")
 public class MobController {
     private final MobService mobService;
