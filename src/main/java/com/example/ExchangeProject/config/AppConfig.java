4f49da069bb7721924b94552e227b284cec32555
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2024-07-21 15:21:20 +0900

    feat: AppConfig 수정 - Timeout error 처리 로직 추가 (RestTemplate)

--- src/main/java/com/example/ExchangeProject/config/AppConfig.java
+++ src/main/java/com/example/ExchangeProject/config/AppConfig.java
@@ -2,6 +2,8 @@ package com.example.ExchangeProject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +11,14 @@ public class AppConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplate(clientHttpRequestFactory());
+    }
+
+    // Timeout error 처리
+    private ClientHttpRequestFactory clientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(5000); // 연결 타임아웃 5초
+        factory.setReadTimeout(5000);    // 읽기 타임아웃 5초
+        return factory;
     }
 }
